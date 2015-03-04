9e8ca5be72ef30b255d4fffa7dfb9dba1de45db9
unknown <strunov@strunov.vrn.dataart.net>
2015-03-04 17:11:27 +0300

    compleete hello world

--- src/samples/Main.java
+++ src/samples/Main.java
@@ -1,18 +1,25 @@
 package samples;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.Sys;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWKeyCallback;
 import org.lwjgl.glfw.GLFWvidmode;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GLContext;
 import org.lwjgl.system.MemoryUtil;
 
 import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
 
 import static org.lwjgl.glfw.Callbacks.errorCallbackPrint;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL32.GL_GEOMETRY_SHADER;
 
 public class Main {
     // We need to strongly reference callback instances.
@@ -21,6 +28,16 @@ public class Main {
 
     // The window handle
     private long window;
+
+    private int positionBufferObject;
+
+    private int theProgram;
+
+    private final float[] vertexPositions = {
+            0.5f, 0.75f, 0.0f, 1.0f,
+            0.75f, -0.75f, 0.0f, 1.0f,
+            -0.75f, -0.75f, 0.0f, 1.0f
+    };
 
     public void run() {
         System.out.println("Hello LWJGL " + Sys.getVersion() + "!");
@@ -45,7 +62,7 @@ public class Main {
         glfwSetErrorCallback(errorCallback = errorCallbackPrint(System.err));
 
         // Initialize GLFW. Most GLFW functions will not work before doing this.
-        if ( glfwInit() != GL11.GL_TRUE )
+        if (glfwInit() != GL11.GL_TRUE)
             throw new IllegalStateException("Unable to initialize GLFW");
 
         // Configure our window
@@ -58,15 +75,15 @@ public class Main {
 
         // Create the window
         window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", MemoryUtil.NULL, MemoryUtil.NULL);
-        if ( window == MemoryUtil.NULL )
+        if (window == MemoryUtil.NULL)
             throw new RuntimeException("Failed to create the GLFW window");
 
         // Setup a key callback. It will be called every time a key is pressed, repeated or released.
         glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
             @Override
             public void invoke(long window, int key, int scancode, int action, int mods) {
-                System.out.println(window+" "+key+" "+scancode+" "+action+" "+mods);
-                if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+                System.out.println(window + " " + key + " " + scancode + " " + action + " " + mods);
+                if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
                     glfwSetWindowShouldClose(window, GL11.GL_TRUE); // We will detect this in our rendering loop
             }
         });
@@ -89,6 +106,99 @@ public class Main {
         glfwShowWindow(window);
     }
 
+    private final String vertexShader =
+            "#version 330\n" +
+                    "layout(location = 0) in vec4 position;\n" +
+                    "void main()\n" +
+                    "{\n" +
+                    "   gl_Position = position;\n" +
+                    "}\n";
+    private final String fragmentShader =
+            "#version 330\n" +
+                    "out vec4 outputColor;\n" +
+                    "void main()\n" +
+                    "{\n" +
+                    "   outputColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n" +
+                    "}\n";
+
+    private void initializeProgram() {
+        ArrayList<Integer> shaderList = new ArrayList<>();
+        shaderList.add( createShader( GL_VERTEX_SHADER, vertexShader ) );
+        shaderList.add( createShader( GL_FRAGMENT_SHADER, fragmentShader ) );
+
+        theProgram = createProgram( shaderList );
+
+        for ( Integer shader : shaderList ) {
+            glDeleteShader( shader );
+        }
+    }
+
+    private int createShader(int shaderType, String shaderFile) {
+        int shader = glCreateShader( shaderType );
+        glShaderSource( shader, shaderFile );
+
+        glCompileShader( shader );
+
+        int status = glGetShaderi( shader, GL_COMPILE_STATUS );
+        if ( status == GL_FALSE ) {
+            int infoLogLength = glGetShaderi( shader, GL_INFO_LOG_LENGTH );
+
+            String infoLog = glGetShaderInfoLog( shader, infoLogLength );
+
+            String shaderTypeStr = null;
+            switch ( shaderType ) {
+                case GL_VERTEX_SHADER:
+                    shaderTypeStr = "vertex";
+                    break;
+                case GL_GEOMETRY_SHADER:
+                    shaderTypeStr = "geometry";
+                    break;
+                case GL_FRAGMENT_SHADER:
+                    shaderTypeStr = "fragment";
+                    break;
+            }
+
+            System.err.printf( "Compile failure in %s shader:\n%s\n", shaderTypeStr, infoLog.trim() );
+        }
+
+        return shader;
+    }
+
+    private int createProgram(ArrayList<Integer> shaderList) {
+        int program = glCreateProgram();
+
+        for ( Integer shader : shaderList ) {
+            glAttachShader( program, shader );
+        }
+
+        glLinkProgram( program );
+
+        int status = glGetProgrami( program, GL_LINK_STATUS );
+        if ( status == GL_FALSE ) {
+            int infoLogLength = glGetProgrami( program, GL_INFO_LOG_LENGTH );
+
+            String infoLog = glGetProgramInfoLog( program, infoLogLength );
+            System.err.printf( "Linker failure: %s\n", infoLog.trim() );
+        }
+
+        for ( Integer shader : shaderList ) {
+            glDetachShader( program, shader );
+        }
+
+        return program;
+    }
+
+    private void initializeVertexBuffer() {
+        FloatBuffer vertexPositionsBuffer = BufferUtils.createFloatBuffer(vertexPositions.length);
+        vertexPositionsBuffer.put(vertexPositions);
+        vertexPositionsBuffer.flip();
+
+        positionBufferObject = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
+        glBufferData(GL_ARRAY_BUFFER, vertexPositionsBuffer, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+
     private void loop() {
         // This line is critical for LWJGL's interoperation with GLFW's
         // OpenGL context, or any context that is managed externally.
@@ -100,10 +210,24 @@ public class Main {
         // Set the clear color
         glClearColor(1.0f, 1.0f, 0.0f, 0.0f);
 
+        initializeProgram();
+        initializeVertexBuffer();
+
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
-        while ( glfwWindowShouldClose(window) == GL_FALSE ) {
+        while (glfwWindowShouldClose(window) == GL_FALSE) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+            glUseProgram(theProgram);
+
+            glBindBuffer(GL15.GL_ARRAY_BUFFER, positionBufferObject);
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(0, 4, GL_FLOAT, false, 0, 0);
+
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+
+            glDisableVertexAttribArray(0);
+            glUseProgram(0);
 
             glfwSwapBuffers(window); // swap the color buffers
 
